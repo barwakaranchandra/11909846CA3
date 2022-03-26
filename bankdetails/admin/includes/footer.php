3ffb3e6333dffe5d787e8b447f3fb0981983a7e3
Karan Barwa <54958197+barwakaranchandra@users.noreply.github.com>
2022-03-26 18:15:24 +0530

    Add files via upload

---
+++ bankdetails/admin/includes/footer.php
@@ -0,0 +1,6 @@
+
+            <!-- Footer -->
+            <footer class="footer">
+              <p>2022 Bank Details by Karan Barwa - 11909846</p>
+            </footer>
+            <!-- End Footer -->
